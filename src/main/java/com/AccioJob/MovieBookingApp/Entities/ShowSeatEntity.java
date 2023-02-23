eaf16cbba5c13b1156c4b2da672ef209f0668344
Abilash S L <abilash0045@gmail.com>
2023-02-23 11:07:58 +0530

    Add Movie-Booking-App

---
+++ src/main/java/com/AccioJob/MovieBookingApp/Entities/ShowSeatEntity.java
@@ -0,0 +1,4 @@
+package com.AccioJob.MovieBookingApp.Entities;
+
+public class ShowSeatEntity {
+}
---
+++ src/main/java/com/AccioJob/MovieBookingApp/MovieBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.AccioJob.MovieBookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBookingAppApplication.class, args);
+	}
+
+}
